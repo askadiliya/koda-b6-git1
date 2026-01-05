0a43a15c0c3fb265e18807b97704e29de5c08feb
askadiliya <askadiliyan@gmail.com>
2026-01-05 17:48:22 +0700

    feat: add feature konversi duhu celcius to fahrenheit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { temperatur } from './temperatur.js';
+
+temperatur.temperaturBase = 25;
+
+console.log("Fahrenheit:", temperatur.toFahrenheit());
+
---
+++ temperatur.js
@@ -0,0 +1,7 @@
+export const temperatur = {
+    temperaturBase: 10, 
+
+    toFahrenheit() {
+        return this.temperaturBase * 9 / 5 + 32;
+    }
+};
