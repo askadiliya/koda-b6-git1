53937f04351baaa5144991c115b9cbcfc1bb4f2e
askadiliya <askadiliyan@gmail.com>
2026-01-05 21:41:43 +0700

    feat: add interaktif konversi suhu 1. Celsius ke fahrenheit 2. Celcius ke kelvin 3. Celcius ke reamur

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
+import readline from 'readline';
 import { temperatur } from './temperatur.js';
 
-temperatur.temperaturBase = 25;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log("Fahrenheit:", temperatur.toFahrenheit());
-console.log("Kelvin:", temperatur.toKelvin());
-console.log("Reamur:", temperatur.toReamur());
+function konversi(satuan) {
+    switch(satuan.toLowerCase()) {
+        case 'celcius': return temperatur.toCelcius();
+        case 'fahrenheit': return temperatur.toFahrenheit();
+        case 'kelvin': return temperatur.toKelvin();
+        case 'reamur': return temperatur.toReamur();
+        default: return null;
+    }
+}
+
+function tanyaSuhu() {
+    rl.question('Masukkan suhu: ', input => {
+        const temp = Number(input);
+        if (Number.isNaN(temp)) {
+            console.log('Input harus berupa angka!');
+            return tanyaSuhu();
+        }
+
+        temperatur.temperaturBase = temp;
+        tanyaSatuan();
+    });
+}
+
+function tanyaSatuan() {
+    rl.question('Masukkan satuan tujuan (celcius/fahrenheit/kelvin/reamur): ', input => {
+        const hasil = konversi(input);
+        if (hasil === null) {
+            console.log('Satuan tidak valid!');
+            return tanyaSatuan();
+        }
+
+        console.log(`Hasil konversi: ${hasil} ${input.toUpperCase()}`);
+        tanyaLagi();
+    });
+}
+
+function tanyaLagi() {
+    rl.question('Mau konversi lagi? (y/n): ', jawaban => {
+        if (jawaban.toLowerCase() === 'y') {
+            tanyaSuhu();
+        } else {
+            console.log('Terima kasih!');
+            rl.close();
+        }
+    });
+}
+
+tanyaSuhu();
